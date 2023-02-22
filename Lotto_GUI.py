e9975ec9bb7251a245201991a4d8ea369ddd8157
MatchaLatte88 <126057372+MatchaLatte88@users.noreply.github.com>
2023-02-22 16:00:52 +0100

    Update Lotto_GUI.py

--- Lotto_GUI.py
+++ Lotto_GUI.py
@@ -2,68 +2,74 @@ import tkinter
 import Lotto as lt
 import customtkinter as ctk # <- import the CustomTkinter module
 
+bg_color_l = "#E4F4F3"
+bg_color_d = "#3E4453"
+#"#6CA8FF"
 #--------------------------base-------------------------------------
 root_tk = tkinter.Tk()  # create the Tk window like you normally do
 root_tk.geometry("400x500")
 root_tk.title("Freds awesome Lucky Number Generator")
-root_tk.config(bg="#282c36")
+root_tk.config(bg=bg_color_l)
 
-ctk.set_appearance_mode("Dark") # Other: "Light", "System" (only macOS)
+ctk.set_appearance_mode("Light") # Other: "Light", "System" (only macOS)
 
 
 #--------------------------FUNCTIONS------------------------------
 
 def eurofct():
-    zz_text.configure(text="Zusatzzahlen")
+    label_zusatz.configure(text="Zusatzzahlen")
     zz_output = lt.Zusatzzahlen.go()
     if type(zz_output) == str:
-        zz_out.configure(text=zz_output)
+        label_zz_output.configure(text=zz_output)
     elif type(zz_output) == list:
-        zz_out.configure(text=(", ".join(map(str, lt.Zusatzzahlen.go()))))
-    hz_text.configure(text="Hauptzahlen")
+        label_zz_output.configure(text=(", ".join(map(str, lt.Zusatzzahlen.go()))))
+    label_hauptz.configure(text="Hauptzahlen")
     hh_output = lt.Hauptzahlen.go()
     if type(hh_output) == str:
-        hz_out.configure(text=hh_output)
+        label_hz_output.configure(text=hh_output)
     elif type(hh_output) == list:
-        hz_out.configure(text=(", ".join(map(str, lt.Hauptzahlen.go()))))
+        label_hz_output.configure(text=(", ".join(map(str, lt.Hauptzahlen.go()))))
 
 def hausfct():
-    th_text.configure(text="Traumhaus Lotterie")
+    label_haus.configure(text="Traumhaus Lotterie")
     th_output = lt.Traumhaus.go()
     if type(th_output) == str:
-        th_out.configure(text=th_output)
+        label_th_output.configure(text=th_output)
     elif type(th_output) == list:
-        th_out.configure(text=(", ".join(map(str, lt.Traumhaus.go()))))
+        label_th_output.configure(text=(", ".join(map(str, lt.Traumhaus.go()))))
 
 def toggle_mode():
     global mode
     current_mode = ctk.get_appearance_mode()
-    if current_mode == "Light":
-        ctk.set_appearance_mode("Dark")
-        root_tk.config(bg="#282c36")
+    if current_mode == "Dark":
+        ctk.set_appearance_mode("Light")
+        root_tk.config(bg=bg_color_l)
         toggle_mode.configure(text="Dark")
     else:
-        ctk.set_appearance_mode("Light")
-        root_tk.config(bg="#ffffff")
+        ctk.set_appearance_mode("Dark")
+        root_tk.config(bg=bg_color_d)
         toggle_mode.configure(text="Light")
 
 #---------------------------check functions----------------------------
 
 #--------------------------Color Variables-------------------------------------
 
+primary_color_light = "#00B6BC"
+primary_color_dark = "#324D5B"
+secondary_color_light = "#71D5E4"
+secondary_color_dark = "#517D94"
+acc_color_light = "#A1DBF1"
+acc_color_dark = "#3ad89d"
+acc2_color_light = "#003739"
+acc2_color_dark = "#B5FDFF"
 
-primary_color_light = "#6CFFC3"
-primary_color_dark = "#000000"
-secondary_color_light = "#6CF1FF"
-secondary_color_dark = "#000000"
-text_color_light = "#ffffff"
-text_color_dark = "#ffffff"
-border_color_light = "#ccd1e0"
-border_color_dark = "#000000"
-background_color_light = "#ffffff"
-background_color_dark = "#000000"
-foreground_color_light = "#000000"
-foreground_color_dark = "#000000"
+""" primary_color_light = "#27C289"
+primary_color_dark = "#324D5B"
+secondary_color_light = "#43697C"
+secondary_color_dark = "#517D94"
+acc_color_light = "#517D94"
+acc_color_dark = "#3ad89d" """
+
 
 
 #------------------------------------BUTTONS------------------------------------
@@ -75,16 +81,23 @@ def create_button(fct, txt, x_pos, y_pos, **attrs):
     color_sl = attrs.get("color_sl", secondary_color_light)
     color_sd = attrs.get("color_sd", secondary_color_dark)
     width = attrs.get("width", 160)
+    height = attrs.get("height", 32)
+    color_txt_l = attrs.get("color_txt_l", "white")
+    color_txt_d = attrs.get("color_txt_d", "white")
+    border = attrs.get("border", 0)
+    corner = attrs.get("corner", 10)
+    border_color = attrs.get("border_color", (acc_color_light ,acc_color_dark))
     button = ctk.CTkButton(master=root_tk,
                                     command=fct,
                                     fg_color=(color_pl,color_pd),
-                                    text_color=("black", "white"),
+                                    text_color=(color_txt_l, color_txt_d),
                                     text=txt,
                                     hover_color=(color_sl, color_sd),
                                     width=width,
-                                    height=32,
-                                    border_width=0,
-                                    corner_radius=10)
+                                    height=height,
+                                    border_width=border,
+                                    border_color=border_color,
+                                    corner_radius=corner)
     button.place(relx=x_pos, rely=y_pos, anchor=tkinter.CENTER)
     return button
 
@@ -99,6 +112,8 @@ def create_frame(x_pos, y_pos, **attrs):
     height = attrs.get("height", 20)
     corner_radius = attrs.get("corner_radius", 0)
     border_width = attrs.get("border_width", 0)
+    color_txt_l = attrs.get("color_txt_l", "white")
+    color_txt_d = attrs.get("color_txt_d", "white")
 
 
     frame = ctk.CTkFrame(master=root_tk,
@@ -106,6 +121,7 @@ def create_frame(x_pos, y_pos, **attrs):
                                width=width,
                                height=height,
                                corner_radius=corner_radius,
+                               border_color=(acc_color_light, acc_color_dark),
                                border_width=border_width)
     frame.place(relx=x_pos, rely=y_pos, anchor=tkinter.CENTER)
     return frame
@@ -115,66 +131,71 @@ def create_frame(x_pos, y_pos, **attrs):
 
 #------------------------------------Labels----------------------------------
 
-def create_label(master, text, x_pos, y_pos, **attrs):
+def create_label(text, x_pos, y_pos, **attrs):
+    master = attrs.get("master", root_tk)
+    anchor = attrs.get("anchor", tkinter.CENTER)
+    color_txt_l = attrs.get("color_txt_l", "black")
+    color_txt_d = attrs.get("color_txt_d", "white")
     label = ctk.CTkLabel(master=master,                                     # master = übergeordnetes Objekt
                       text=text,
-                      font=("Helvetica", 16))
-    label.place(relx=x_pos, rely=y_pos, anchor=tkinter.CENTER)
+                      text_color=(color_txt_l, color_txt_d),
+                      width=90,
+                      height=25,
+                      font=("Helvetica", 12))
+    label.place(relx=x_pos, rely=y_pos, anchor=anchor)
     return label
 
 
+#______________________Building GUI_______________________________
+
+header              = create_frame(x_pos=0.5, y_pos=0.0, color_pl="#A1DBF1", height=60)  ##HEADER
+label_header        = create_label("GC Number Generator", 0.5, 0.75, master=header, color_txt_l=acc2_color_light, color_txt_d=acc2_color_dark)
+
+frame_lotto        = create_frame(x_pos=0.5, y_pos=0.355, color_pl=bg_color_l, color_pd=bg_color_d, height=70, width=250, corner_radius=10, border_width=1)  #Output Rahmen Lotto
+frame_traumhaus    = create_frame(x_pos=0.5, y_pos=0.755, color_pl=bg_color_l, color_pd=bg_color_d, height=70, width=250, corner_radius=10, border_width=1)  #Output Rahmen Haus
+
+toggle_mode_A   = create_button(toggle_mode,
+                              " ",
+                              0.05,
+                              0.11,
+                              width = 15,
+                              height = 5,
+                              color_pd=bg_color_l,
+                              color_pl=bg_color_d,
+                              border=1,
+                              border_color=(bg_color_d,bg_color_l),
+                              corner=0)
+toggle_mode_B   = create_button(toggle_mode,
+                              " ",
+                              0.08,
+                              0.11,
+                              width = 15,
+                              height = 5,
+                              color_pd=bg_color_d,
+                              color_pl=bg_color_l,
+                              border=1,
+                              border_color=(bg_color_d,bg_color_l),
+                              corner=0)
+
+button_lotto        = create_button(eurofct, "Press here to win millions!", 0.5, 0.2, border=1)
+button_traumhaus    = create_button(hausfct, "Press here to win a Dreamhouse!", 0.5, 0.6)
 
 
+#_______________________Hauptzahlen________________________________
 
-
-#______________________Hauptzahlen_______________________________
-
-header              = create_frame(x_pos=0.5, y_pos=0.0, color_pl="#6CA8FF", height=120)  ##HEADER
-
-rahmen_lotto        = create_frame(x_pos=0.5, y_pos=0.355, color_pl="white", height=70, width=250, corner_radius=10, border_width=2)  #Output Rahmen Lotto
-rahmen_traumhaus    = create_frame(x_pos=0.5, y_pos=0.655, color_pl="white", height=70, width=250, corner_radius=10, border_width=2)  #Output Rahmen Haus
-
-
-toggle_mode   = create_button(toggle_mode, "dark", 0.1, 0.18, width = 50)
-#button_lotto        = create_button(eurofct, "Press here to win millions!", 0.5, 0.2)
-button_traumhaus    = create_button(hausfct, "Press here to win a Dreamhouse!", 0.5, 0.5)
-
-label_header        = create_label(header, "GC Number Generator", 0.5, 0.75)
-
-hz_out = ctk.CTkLabel(master=root_tk,
-                               text="xxxxx",
-                               width=90,
-                               height=25)
-hz_out.place(relx=0.47, rely=0.39, anchor=tkinter.E)
+label_hauptz        = create_label("Hauptzahlen:", 0.47, 0.32, anchor=tkinter.E)
+label_hz_output = create_label("xxxxxxbxx", 0.47, 0.39, anchor=tkinter.E)
 
 #______________________Zusatzzahlen_______________________________
 
-zz_text = ctk.CTkLabel(master=root_tk,
-                               text="Zusatzzahlen:",
-                               width=90,
-                               height=25)
-zz_text.place(relx=0.53, rely=0.32, anchor=tkinter.W)
-
-zz_out = ctk.CTkLabel(master=root_tk,
-                               text="xx",
-                               width=90,
-                               height=25)
-zz_out.place(relx=0.53, rely=0.39, anchor=tkinter.W)
+label_zusatz        = create_label("Zusatzzahlen:", 0.53, 0.32, anchor=tkinter.W)
+label_zz_output = create_label("xx", 0.53, 0.39, anchor=tkinter.W)
 
 #______________________Traumhaus_______________________________
+label_haus        = create_label("Zusatzzahlen:", 0.5, 0.72)
+label_th_output = create_label("xx", 0.5, 0.79)
 
 
-th_text = ctk.CTkLabel(master=root_tk,
-                               text="Taumhaus Losnummer:",
-                               width=120,
-                               height=25)
-th_text.place(relx=0.5, rely=0.62, anchor=tkinter.CENTER)
-
-th_out = ctk.CTkLabel(master=root_tk,
-                               text="XXXXXXXXXXXXX",
-                               width=120,
-                               height=25)
-th_out.place(relx=0.5, rely=0.69, anchor=tkinter.CENTER)
 #======================================================
 root_tk.mainloop()
 
